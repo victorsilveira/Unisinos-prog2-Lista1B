b4b9a41573850f029e7e20bcb318063c29362c61
victorsilveira <victorsilveira22@gmail.com>
2020-08-14 21:25:48 -0300

    Add the class Pessoa to solve the no4

---
+++ Pessoa.java
@@ -0,0 +1,11 @@
+package Lista1BDeExercicios;
+
+abstract class Pessoa {
+
+    /*
+    Questão 4
+    O metodo abstrato sempre vai estar representado como abaixo na classe abstrata.
+    Pois na classe que herdar esta deve ser implementado o método com as determinadas funções.
+     */
+    public abstract void exibeDados();
+}
